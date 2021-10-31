b265bda884c8b8e303e72a12d3831ca06e827973
jeff chen <chenjeff4840@vt.edu>
2021-10-31 19:14:46 -0400

    InputBuffer completed and fully rigorously tested

--- src/InputBuffer.java
+++ src/InputBuffer.java
@@ -25,8 +25,8 @@ import java.nio.ByteBuffer;
 // Java Doc ------------------------------------------------------------------
 /**
  * Input buffer of X size, able to read and manage a RandomAccessFile, Byte[],
- * and
- * ByteBuffer.
+ * and ByteBuffer. Buffer block size fixed to 8192 bytes, cannot be modified
+ * due to specialized and fast block calculation algorithm.
  * 
  * @author chenj (chenjeff4840)
  * @version 10.30.2021
@@ -42,14 +42,12 @@ public class InputBuffer {
     /**
      * Initializes fields
      * 
-     * @param bytes
-     *            Byte size of buffer
      * @param file
      *            file to be managed
      * @throws IOException
      */
-    public InputBuffer(int bytes, RandomAccessFile file) throws IOException {
-        data = new byte[bytes];
+    public InputBuffer(RandomAccessFile file) throws IOException {
+        data = new byte[8192];
         this.file = file;
         file.read(data);
         buffer = ByteBuffer.wrap(data);
@@ -72,34 +70,112 @@ public class InputBuffer {
 
 
     /**
+     * Increments buffer by increment bytes
+     * 
+     * @param increment
+     *            bytes to increment buffer
+     */
+    public boolean next(int increment) {
+        try {
+            buffer.position(buffer.position() + increment);
+        }
+        catch (IllegalArgumentException E) {
+            return false;
+        }
+        return true;
+    }
+
+
+    /**
+     * Reads next length bytes in buffer, returns as a double. Increment buffer
+     * by length bytes.
+     * 
+     * @param length
+     *            # of bytes to be read buffer
+     * @return read bytes as double
+     */
+    public double nextDouble(int length) {
+        byte[] temp = new byte[length];
+        this.next(temp);
+        return this.convertToDouble(temp);
+    }
+
+
+    /**
+     * Reads next length bytes in buffer, returns as a long. Increments buffer
+     * by
+     * length bytes.
+     * 
+     * @param length
+     *            # of bytes to be read buffer
+     * @return read bytes as long
+     */
+    public long nextLong(int length) {
+        byte[] temp = new byte[length];
+        this.next(temp);
+        return this.convertToLong(temp);
+    }
+
+
+    /**
      * Moves the buffer to the beginning of the next block in the file where a
-     * block is of size data.length
+     * block is of size data.length. If the buffer is currently inside 2 blocks,
+     * the buffer will move to the next adjacent block.
+     * 
+     * File position will be unchanged if filePointer is at or past EOF.
      * 
      * @throws IOException
      */
     public void nextBlock() throws IOException {
-        file.seek(data.length + file.getFilePointer());
+        // Right shift == FilePointer // 8192
+        // -1 is to push the filePointer back into the current block
+        long curr = (file.getFilePointer() - 1) >> 13;
+
+        // Left shift == quotient * 8192
+        curr = curr << 13;
+
+        //
+        this.seek(curr + data.length);
     }
 
 
     /**
-     * Moves buffer to position pos in the file
+     * Moves buffer to position pos in the file with RandomAccessFile.seek().
+     * Does nothing if seek is < 0. Seek can be set beyond the end of the file;
+     * however, file position will remain unchanged.
+     * 
+     * If there is not enough data left in the file to read into the array, old
+     * data may still persist:
+     * 
+     * Example:
+     * File bytes left after seek = 300
+     * buffer position 0->299 new data
+     * buffer position 300-8191 old data
+     * 
+     * Although seek cannot allow parameters greater than file length, the
+     * file pointer may still skip past the end of the file if parameter
+     * given is such that there are less than 8192 bytes left past the
+     * seek position, should not affect program as counted as EOF.
      * 
      * @param pos
-     *            Position to move buffer to in file
+     *            Position to move buffer to in file, will not affect file
+     *            pointer
+     *            is < 0 || >= file.length
      * @throws IOException
      */
     public void seek(long pos) throws IOException {
-        file.seek(pos);
-        file.read(data);
-        buffer.rewind();
+        if (pos >= 0 && pos < file.length()) {
+            file.seek(pos);
+            file.read(data);
+            buffer.rewind();
+        }
     }
 
 
     /**
-     * Returns entirety of the current block
+     * Returns shallow copy of data from the current buffer
      * 
-     * @return entirety of the current block
+     * @return shallow copy of data from current buffer
      */
     public byte[] getData() {
         return data;
@@ -129,6 +205,8 @@ public class InputBuffer {
 
     /**
      * Resets the buffer with ByteBuffer.reset()
+     * 
+     * @precondition mark exists
      */
     public void reset() {
         buffer.reset();
@@ -140,5 +218,43 @@ public class InputBuffer {
      */
     public void mark() {
         buffer.mark();
+    }
+
+
+    /**
+     * Checks if EOF
+     * 
+     * @return true if file is at EOF or past, false otherwise
+     * @throws IOException
+     */
+    public boolean endOfFile() throws IOException {
+        return file.getFilePointer() >= file.length();
+    }
+
+
+    // Helpers --------------------------------------------------------------
+    /**
+     * Converts array to long
+     * 
+     * @param array
+     *            Array to convert to long
+     * @return array as long
+     */
+    private long convertToLong(byte[] array) {
+        ByteBuffer buffer = ByteBuffer.wrap(array);
+        return buffer.getLong();
+    }
+
+
+    /**
+     * Converts array to double
+     * 
+     * @param array
+     *            Array to convert to double
+     * @return array as double
+     */
+    private double convertToDouble(byte[] array) {
+        ByteBuffer buffer = ByteBuffer.wrap(array);
+        return buffer.getDouble();
     }
 }
--- src/InputBufferTest.java
+++ src/InputBufferTest.java
@@ -1,3 +1,4 @@
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import org.junit.Test;
@@ -30,29 +31,112 @@ import student.TestCase;
  * @author chenj (chenjeff4840)
  * @version 10.30.2021
  */
-public class InputBufferTest  extends TestCase{
-    
+public class InputBufferTest extends TestCase {
+
     // Fields ---------------------------------------------------------------
     private InputBuffer buffer;
-    
+
+    // Setup ------------------------------------------------------------
+    /**
+     * Creates buffer with size 8192 bytes and Canvas provided input file
+     */
     @Test
     public void setUp() throws IOException {
         RandomAccessFile file = new RandomAccessFile("sampleInput16.bin", "r");
-        buffer = new InputBuffer(8192, file);
-        System.out.println("Running???");
+        buffer = new InputBuffer(file);
+    }
+
+
+    // Tests ------------------------------------------------------------
+    /**
+     * Tests nextBlock(), nextLong(), nextDouble() in real world
+     * applications with a sample file
+     * 
+     * @throws IOException
+     */
+    public void testNextSamples() throws IOException {
+        // Reading every key and value in sample output file
+        assertEquals(5859826799363951096L, buffer.nextLong(8));
+        assertEquals(7.25837957933813E-309, buffer.nextDouble(8), 0.1);
+        buffer.nextBlock();
+        assertEquals(872093003042532807L, buffer.nextLong(8));
+        assertEquals(2.846974648265778E-271, buffer.nextDouble(8), 0.1);
+        buffer.nextBlock();
+        assertEquals(4746048651426934305L, buffer.nextLong(8));
+        assertEquals(8.021302838493087E-236, buffer.nextDouble(8), 0.1);
+        buffer.nextBlock();
+        assertEquals(2465224465483701295L, buffer.nextLong(8));
+        assertEquals(1.979063847945134E-200, buffer.nextDouble(8), 0.1);
+        buffer.nextBlock();
+        assertEquals(1050792465528211139L, buffer.nextLong(8));
+        assertEquals(1.9121111284579667E-165, buffer.nextDouble(8), 0.1);
+        buffer.nextBlock();
+        assertEquals(6050394105966916791L, buffer.nextLong(8));
+        assertEquals(2.2317027604113507E-127, buffer.nextDouble(8), 0.1);
+        buffer.nextBlock();
+        assertEquals(1026023591337815624L, buffer.nextLong(8));
+        assertEquals(1.5574815733570753E-91, buffer.nextDouble(8), 0.1);
+        buffer.nextBlock();
+        assertEquals(3727109532527581177L, buffer.nextLong(8));
+        assertEquals(7.578200413844949E-58, buffer.nextDouble(8), 0.1);
+        buffer.nextBlock();
+        assertEquals(1666373987716394526L, buffer.nextLong(8));
+        assertEquals(1.948647168795557E-21, buffer.nextDouble(8), 0.1);
+        buffer.nextBlock();
+        assertEquals(2109762501594140130L, buffer.nextLong(8));
+        assertEquals(4.7295568637570205E12, buffer.nextDouble(8), 0.1);
+        buffer.nextBlock();
+        assertEquals(704373661941503400L, buffer.nextLong(8));
+        assertEquals(2.8332973775294907E48, buffer.nextDouble(8), 0.1);
+        buffer.nextBlock();
+        assertEquals(168353935316054591L, buffer.nextLong(8));
+        assertEquals(7.423511124391644E81, buffer.nextDouble(8), 0.1);
+        buffer.nextBlock();
+        assertEquals(1813093945163867404L, buffer.nextLong(8));
+        assertEquals(1.123748335113702E114, buffer.nextDouble(8), 0.1);
+        buffer.nextBlock();
+        assertEquals(6011240381167188375L, buffer.nextLong(8));
+        assertEquals(5.443479801473815E147, buffer.nextDouble(8), 0.1);
+        buffer.nextBlock();
+        assertEquals(8334502261472908423L, buffer.nextLong(8));
+        assertEquals(6.228961731759273E184, buffer.nextDouble(8), 0.1);
+        buffer.nextBlock();
+        assertEquals(4290883147614596L, buffer.nextLong(8));
+        assertEquals(1.206088797278413E221, buffer.nextDouble(8), 0.1);
+        buffer.nextBlock();
+
+        // At the end of the file now
+        buffer.next(8193);
 
     }
-    
+
+
     /**
-     * Reads the next dest.length bytes in buffer and saves it to dest.
-     * Increments
-     * buffer forward by dest.length bytes.
+     * Playing with a tiny bin with total bytes < 8192
      * 
-     * @param dest
-     *            byte[] to save dest.length bytes to
+     * @throws IOException
+     * @throws FileNotFoundException
      */
-    public void testNext(byte[] dest) {
-        System.out.println("Running???");
+    public void testTiny() throws FileNotFoundException, IOException {
+        // Initializing ---------------------------------------------------
+        InputBuffer buff = new InputBuffer(new RandomAccessFile("tiny.bin",
+            "r"));
+
+        // Taking byte[] info
+        byte[] data = buff.getData();
+
+        // Test confirms byte[] start with an array of 0s (NULL in ASCII).
+        // Reusing an old array to be filled with a not full block leaves old
+        // data from the previous block in the reused array. This can be
+        // remedied
+        // with a clearing function; however, that is beyond the scope of this
+        // project since all blocks will always be 8192 byte size.
+        assertEquals(0, data[304]);
+        assertFalse(0 == data[303]);
+
+        buffer.changeFile(new RandomAccessFile("tiny.bin", "r"));
+        assertEquals(data[303], buffer.getData()[303]);
+        assertFalse(0 == buffer.getData()[304]);
     }
 
 
@@ -62,27 +146,109 @@ public class InputBufferTest  extends TestCase{
      * 
      * @throws IOException
      */
-    public void nextBlock() throws IOException {
+    public void testNextBlock() throws IOException {
+        // Normal nextBlock()
+        buffer.nextBlock();
+        assertEquals(872093003042532807L, buffer.nextLong(8));
+
+        // nextBlock at EOF -------------------------------------------
+        buffer.seek(122880);
+
+        // Verify
+        assertEquals(4290883147614596L, buffer.nextLong(8));
+
+        // Calling nextBlock to go through EOF -----------------------
+        buffer.nextBlock();
+
+        // Verify that buffer is still the current position
+        assertEquals(1.206088797278413E221, buffer.nextDouble(8), 0.1);
+
+        // nextBlock while at middle of 2 existing blocks ------------
+        buffer.seek(4096);
+        buffer.nextBlock();
+        assertEquals(4746048651426934305L, buffer.nextLong(8));
+        assertEquals(8.021302838493087E-236, buffer.nextDouble(8), 0.1);
+        buffer.nextBlock();
+
+        // Return to previous position then next block ----------------
+        buffer.seek(0);
+        buffer.nextBlock();
+        assertEquals(872093003042532807L, buffer.nextLong(8));
+        assertEquals(2.846974648265778E-271, buffer.nextDouble(8), 0.1);
+
+        // Negative seek then nextBlock --------------------------------
+        buffer.seek(-12312);
+        buffer.nextBlock();
+        assertEquals(4746048651426934305L, buffer.nextLong(8));
+        assertEquals(8.021302838493087E-236, buffer.nextDouble(8), 0.1);
+
+        // Seeking to past EOF and then calling nextBlock ---------------
+        buffer.seek(131073);
+        buffer.nextBlock();
+        assertEquals(2465224465483701295L, buffer.nextLong(8));
+        assertEquals(1.979063847945134E-200, buffer.nextDouble(8), 0.1);
     }
 
 
     /**
-     * Moves buffer to position pos in the file
-     * 
-     * @param pos
-     *            Position to move buffer to in file
-     * @throws IOException
+     * tests seek
      */
-    public void seek(long pos) throws IOException {
+    public void testSeek() throws IOException {
+        // Negative seek ---------------------------------------------
+        buffer.seek(-1);
+
+        // Seek past EOF ---------------------------------------------
+        // Seek to key in first block, verify location
+        buffer.seek(8);
+        assertEquals(7.25837957933813E-309, buffer.nextDouble(8), 0.1);
+
+        // Seek past EOF, verify file location has not changed
+        buffer.seek(546546545646L);
+        assertEquals(7.25837957933813E-309, buffer.nextDouble(8), 0.1);
+
+        // Seeking to the beginning of the last block -----------------
+        buffer.seek(122880);
+        assertEquals(4290883147614596L, buffer.nextLong(8));
+
+        // Seeking back to the beginning of the file
+        buffer.seek(0);
+        assertEquals(5859826799363951096L, buffer.nextLong(8));
+
+        // Seek last byte in file ---------------------------------------
+        buffer.seek(131071);
+        assertTrue(buffer.next(8192));
+
     }
 
 
     /**
-     * Returns entirety of the current block
-     * 
-     * @return entirety of the current block
+     * Tests next()
      */
-    public void getData() {
+    public void testNext() {
+        // Skipping to the key -----------------------------------------------
+        assertTrue(buffer.next(8));
+
+        // Verifying
+        assertEquals(7.25837957933813E-309, buffer.nextDouble(8), 0.1);
+
+        // Skipping to the end of the buffer
+        assertTrue(buffer.next(8192 - 16));
+
+        // Going out of bounds
+        assertFalse(buffer.next(1));
+    }
+
+
+    /**
+     * test getData()
+     */
+    public void testGetData() {
+        byte[] copy = buffer.getData();
+        copy[0] = 91;
+
+        assertEquals(copy[0], buffer.getData()[0]);
+        copy[1] = 92;
+        assertEquals(copy[0], buffer.getData()[0]);
     }
 
 
@@ -92,29 +258,111 @@ public class InputBufferTest  extends TestCase{
      * @param file
      *            File to change to
      * @throws IOException
+     * @throws FileNotFoundException
      */
-    public void changeFile(RandomAccessFile file) throws IOException {
+    public void testChangeFile() throws FileNotFoundException, IOException {
+        // Saving first 8 bytes of input into a double
+        double a = buffer.nextLong(8);
+
+        // Switching the file
+        buffer.changeFile(new RandomAccessFile("64Blocks_Barnette.bin", "r"));
+
+        // Saving the first 8 bytes of this other file
+        double b = buffer.nextLong(8);
+
+        // Confirming this is indeed the beginning of the other file
+        buffer.rewind();
+        double b2 = buffer.nextLong(8);
+        assertEquals(b, b2, 0.1);
+
+        // Checking if a different file was indeed selected
+        assertFalse(a == b);
+
     }
 
 
     /**
-     * Rewinds the buffer with ByteBuffer.rewind()
+     * test rewind
      */
-    public void rewind() {
+    public void testRewind() {
+        // Saving initial value
+        long original = buffer.nextLong(8);
+
+        // Jumping 4000 bytes in buffer and saving value as long
+        buffer.next(4000);
+        long jump = buffer.nextLong(8);
+
+        // Rewinding and verifying
+        buffer.rewind();
+        long rewind = buffer.nextLong(8);
+
+        assertEquals(original, rewind);
+        assertFalse(jump == rewind);
     }
 
 
     /**
-     * Resets the buffer with ByteBuffer.reset()
+     * test rewind
      */
-    public void reset() {
+    public void testResetMark() {
+        // Saving initial values
+        long begin = buffer.nextLong(8);
+        buffer.mark();
+        long original = buffer.nextLong(8);
+
+        // Jumping 4000 bytes in buffer and saving value as long
+        buffer.next(4000);
+        long jump = buffer.nextLong(8);
+
+        // Resetting and verifying
+        buffer.reset();
+        long reset = buffer.nextLong(8);
+
+        assertEquals(original, reset);
+        assertFalse(jump == reset);
+
+        // Rewinding and making sure mark is gone
+        buffer.rewind();
+        assertEquals(begin, buffer.nextLong(8));
+        assertFalse(begin == reset);
     }
-
-
+    
     /**
-     * Marks the buffer with ByteBuffer.mark()
+     * test endOfFile()
+     * @throws IOException 
      */
-    public void mark() {
+    public void testEndOfFile() throws IOException {
+        // Beginning
+        assertFalse(buffer.endOfFile());
+        
+        // Middle
+        buffer.seek(70000);
+        assertFalse(buffer.endOfFile());
+        
+        // End
+        buffer.seek(122880);
+        assertTrue(buffer.endOfFile());
+        
+        buffer.seek(122879);
+        assertFalse(buffer.endOfFile());
+        
+        // Resetting
+        buffer.seek(0);
+        
+        // Negative "seek logic should prevent this"
+        buffer.seek(-121);
+        assertFalse(buffer.endOfFile());
+        
+        // Past "Only occurs for specific seek positions which cannot occur with 
+        // 8192 byte block size requirement for project"
+        buffer.seek(130000);
+        assertTrue(buffer.endOfFile());
+        
+        // Resetting
+        buffer.seek(0);
+        
+        // Explicitly going way beyond EOF "seek prevents this"
+        buffer.seek(8956565);
+        assertFalse(buffer.endOfFile());
     }
-
 }

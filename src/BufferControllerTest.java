097bcf92271fac11648ad0c5208ad3522ec33697
chenjeff4840 <chenjeff4840@vt.edu>
2021-11-17 19:46:47 -0500

    work in progress

--- src/BufferControllerTest.java
+++ src/BufferControllerTest.java
@@ -7,6 +7,7 @@ public class BufferControllerTest extends student.TestCase {
 
     private BufferController bc;
     private final String OUTPUT = "sampleInput16.bin";
+    private final String OUTPUT2 = "Temp9BreverseSorted.bin";
 
     public void setUp() throws IOException {
         bc = new BufferController(OUTPUT);
@@ -30,16 +31,42 @@ public class BufferControllerTest extends student.TestCase {
         while (!input.endOfFile()) {
             for (int i = 0; i < 206; i++) {
                 assertTrue(prev.compareTo(this.getKey(input)) <= 0);
+                input.next(16);
 
             }
             input.nextBlock();
         }
 
-        // Testing a double run file
         
-        //
-
         input.close();
+        
+        File file = new File("JeffChenRunUno.bin");
+        file.delete();
+        file.createNewFile();
+        
+        // Testing a double run file
+        bc = new BufferController(OUTPUT2);
+        bc.replacementSelection();
+        
+        input = new InputBuffer(new RandomAccessFile(
+            "JeffChenRunUno.bin", "r"));
+
+        prev = this.getKey(input);
+  
+
+        while (!input.endOfFile()) {
+            for (int i = 0; i < 206; i++) {
+                //assertTrue(prev.compareTo(this.getKey(input)) <= 0);
+                System.out.print(this.getKey(input) + " ");
+                input.next(16);
+            }
+            System.out.println("");
+            input.nextBlock();
+        }
+
+        
+        input.close();
+        
     }
 
 
